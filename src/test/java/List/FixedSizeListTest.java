7704e83eaf52ae0e36a6327c414156606985e0ae
Vlad Glv <vladglv@users.noreply.github.com>
2017-10-13 15:11:25 -0400

    Added tests for the FixedSizeList

--- src/test/java/List/FixedSizeListTest.java
+++ src/test/java/List/FixedSizeListTest.java
@@ -1,5 +1,118 @@
 package List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.collections4.list.FixedSizeList;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class FixedSizeListTest {
-    // TODO
+    private FixedSizeList<Integer> a;
+    private List<Integer> listA;
+
+    @After
+    public void destroyObjects() {
+        listA = a = null;
+    }
+
+    @Before
+    public void initObjects() {
+        listA = Arrays.asList(1, 2, 3, 4, 5);
+        a = FixedSizeList.fixedSizeList(listA);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testAdd() {
+        initObjects();
+
+        a.add(new Integer(5));
+
+        destroyObjects();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testAddPosition() {
+        initObjects();
+
+        a.add(0, new Integer(5));
+
+        destroyObjects();
+    }
+
+    @Test
+    public void testContains() {
+        initObjects();
+
+        assertThat(a).contains(1);
+
+        destroyObjects();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testContainsFail() {
+        initObjects();
+
+        assertThat(a).contains(7);
+
+        destroyObjects();
+    }
+
+    @Test
+    public void testEmpty() {
+        initObjects();
+
+        assertThat(a).isNotEmpty();
+
+        destroyObjects();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testEmptyFail() {
+        initObjects();
+
+        assertThat(a).isEmpty();
+
+        destroyObjects();
+    }
+
+    @Test
+    public void testGet() {
+        initObjects();
+
+        assertThat(a).element(1).isEqualTo(2);
+
+        destroyObjects();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void testGetFail() {
+        initObjects();
+
+        assertThat(a).element(1).isEqualTo(3);
+
+        destroyObjects();
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testRemovePosition() {
+        initObjects();
+
+        a.remove(0);
+
+        destroyObjects();
+    }
+
+    @Test
+    public void testSize() {
+        initObjects();
+
+        assertThat(a).hasSize(listA.size());
+
+        destroyObjects();
+    }
+
 }
--- src/test/java/List/ListUtilsTest.java
+++ src/test/java/List/ListUtilsTest.java
@@ -1,5 +1,17 @@
 package List;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+
 public class ListUtilsTest {
-    // TODO
+    private List<Integer> listA;
+    private List<Integer> listB;
+
+    @Before
+    public void initObjects() {
+        listA = Arrays.asList(1, 2, 3, 4, 5);
+        listA = Arrays.asList(3, 4, 5, 6, 7);
+    }
 }
