929d626450fe4e91814f321fc8bbea159c2b174d
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-12-01 22:27:20 -0500

    Gradle + Assertj

---
+++ src/test/TreeBidiMap_test.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import static org.assertj.core.api.Assertions.*;
+
+public class TreeBidiMap_test {
+
+	@Before
+	public void setUp(){
+		
+	}
+
+	@After
+	public void tearDown(){
+		
+	}
+
+	@Test
+	public void test() {
+		assertThat("fuck Eclipse").as("2 == 2").isEqualTo("fuck Eclipse");
+	}
+
+}
